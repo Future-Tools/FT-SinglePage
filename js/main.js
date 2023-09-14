45d69281a8e1a339020a11d87def9f84370eb1dc
Tim McGinley <timwasnever@gmail.com>
2023-09-14 09:35:03 +0200

    added js folder

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var jules = document.getElementById("about").scrollTop;
+    var nules = document.getElementById("method").scrollTop;
+    console.log(jules+'-'+nules+'_'+document.getElementById("method").offsetTop);
+}
